88ef3bef96725631d9a413c38e14567df3e1d763
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-04-27 15:25:59 +0000

    added project title to README.

---
+++ assets/scripts/script.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    runGame("addition");
+});
+
+
+function runGame(gameType) {
+  let num1 =  Math.floor(Math.random() * 25) + 1;
+  let num2 =  Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === "addition") {
+      displayAdditionQuestion(num1, num2);
+  } else {
+      alert(`Unknown game type ${gameType}`);
+      throw `Unknown game type ${gameType}, aborting!`;
+  }
+}
+
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Well Done!");
+    } else {
+        alert(`Boo! You answered: ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
+}
+
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unknown game type ${gameType}`);
+        throw `Unknown game type ${gameType}, aborting!`;
+    }
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operator").textContent = "+";
+    document.getElementById("operand2").textContent = operand2;
+}
+
+function displaySubtractionQuestion() {
+    
+}
+
+function displayMultiplicationQuestion() {
+    
+}
+
+function displayDivisionQuestion() {
+    
+}
